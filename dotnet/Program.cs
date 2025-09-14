b2311d1ed2650f9294869c9284d12c47a4cd9653
DenisNikonorov <nikoden2005@gmail.com>
2025-09-14 21:01:07 +0300

    add files for lab 2

---
+++ dotnet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Figures
+{
+    class Program
+    {
+        static void Main(string[] argv)
+        {
+            // проверка работы конструкторов
+            Rectangle rect1 = new Rectangle(10, 10);
+            Square square1 = new Square(5);
+            Circle circle1 = new Circle(2);
+
+            // проверка метода ToString() для каждой ранее созданной фигуры
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("проверка метода ToString для каждой ранее созданной фигуры");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"rect1: {rect1.ToString()}\nsquare1: {square1.ToString()}\ncircle1: {circle1.ToString()}");
+
+            // проверка интерфейса IPrint на примере круга
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nПроверка интерфейса IPrint на примере круга");
+            Console.ForegroundColor = ConsoleColor.White;
+            IPrint circle2 = new Circle(4);
+            circle2.Print();
+        }
+    }
+} // Figures
