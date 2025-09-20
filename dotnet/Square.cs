ed385b1a7ae1bfa5507f6ba0827ee83ab924a93a
DenisNikonorov <nikoden2005@gmail.com>
2025-09-20 12:00:02 +0300

    update files for lab_2

---
+++ dotnet/Square.cs
@@ -0,0 +1,31 @@
+namespace Figures
+{
+    class Square : Rectangle, IPrint
+    {
+        private double sideSize = 0;
+        public double Side
+        {
+            get { return this.sideSize; }
+            set { this.sideSize = value; }
+        }
+        public Square(int a = 0)
+        {
+            this.sideSize = a;
+        }
+
+        public override double Area()
+        {
+            return this.sideSize * this.sideSize;
+        }
+
+        public override string ToString()
+        {
+            return $"side = {this.sideSize}; area: {Math.Round(this.Area(), 2)}";
+        }
+
+        void IPrint.Print()
+        {
+            Console.WriteLine($"Квадрат: {this.ToString()}");
+        }
+    }
+}
